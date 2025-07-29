2be03c0b43f46e1d2eea0be70c20c08408204fbb
Suyatna <zuyatna@gmail.com>
2025-07-29 20:11:47 +0700

    fix: transaction.go model

--- transaction/model/transaction.go
+++ transaction/model/transaction.go
@@ -1,1 +1,26 @@
 package model
+
+import "time"
+
+type TransactionType string
+
+const (
+	Topup    TransactionType = "topup"
+	Transfer TransactionType = "transfer"
+)
+
+type Transaction struct {
+	ID              string
+	FromAccountID   string
+	ToAccountID     string
+	Amount          float64
+	TransactionType TransactionType
+	Notes           string
+	CreatedAt       time.Time
+}
+
+type Account struct {
+	ID    string
+	Name  string
+	Email string
+}
